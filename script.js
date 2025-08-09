7467871be0e3d68842f1ae78419394d1726d7376
Muhamad Novreysa <43816986+MuhamadNovreysa@users.noreply.github.com>
2025-08-10 02:22:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Navbar JS
+document.addEventListener('DOMContentLoaded', function() {
+  const toggle = document.querySelector('.navbar-toggle');
+  const menu = document.querySelector('.navbar-menu');
+  
+  toggle.addEventListener('click', function() {
+    menu.classList.toggle('active');
+  });
+});
+
 // Typewriter Effect
 function initTypewriter() {
   const text = "SISTEM INFORMASI DESA KARANGHARJA";
